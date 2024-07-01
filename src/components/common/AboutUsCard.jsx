71d3d1c8fd0e2f32c420a0fec08acbafe8aeb316
Mukesh <mkverma97297@gmail.com>
2024-07-01 12:22:47 +0530

    responsive

--- src/components/common/AboutUsCard.jsx
+++ src/components/common/AboutUsCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutUsCard = ({ item }) => {
   return (
     <div className="our-services-card overflow-hidden">
-      <div className="text-white px-4 sm:py-6 p-y-4 rounded-[30px] sm:space-y-5 space-y-3 bg-black">
+      <div className="text-white px-4 sm:py-6 py-4 rounded-[30px] sm:space-y-5 space-y-3 bg-black">
         <h1 className="text-yellow-400 lg:text-6xl text-3xl font-medium lg:text-start text-center font-var-roman">
           {item.heading}
         </h1>
